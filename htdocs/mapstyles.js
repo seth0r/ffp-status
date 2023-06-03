76bfe9a34fbb119e772efe642344fb66cbc95891
root <root@v220230257037219818.happysrv.de>
2023-06-03 16:03:39 +0200

    returning colors as array to change alpha later

--- htdocs/mapstyles.js
+++ htdocs/mapstyles.js
@@ -1,4 +1,4 @@
-const nodecolors = {
+const node_colors = {
     green : [   0, 238,  0, 1.0 ],
     yellow: [ 255, 255,  0, 1.0 ],
     red   : [ 255,   0,  0, 1.0 ],
@@ -7,9 +7,9 @@ const nodecolors = {
 
 function icolor( value, colors ){
     if (value <= colors[0][0]) {
-        return "rgba(" + colors[0].slice(1).join() + ")";
+        return colors[0].slice(1);
     } else if (value >= colors[colors.length-1][0]) {
-        return "rgba(" + colors[colors.length-1].slice(1).join() + ")";
+        return colors[colors.length-1].slice(1);
     }
     for (i=0; i<colors.length-1; i++) {
         if (value >= colors[i][0] && value <= colors[i+1][0]) {
@@ -18,7 +18,7 @@ function icolor( value, colors ){
             for (j=1; j<5; j++){
                 c.push( colors[i][j] + (colors[i+1][j] - colors[i][j]) * f );
             }
-            return "rgba(" + c.join() + ")";
+            return c;
         }
     }
 };
@@ -27,7 +27,7 @@ function style_node(feature,resolution) {
     var props = feature.getProperties();
     var colors = [];
     for ( [cn, offline] of _.sortBy( _.pairs(props['offline_limits']), function(a){ return a[1]} ) ) {
-        colors.push( [offline].concat(nodecolors[cn]) );
+        colors.push( [offline].concat(node_colors[cn]) );
     }
     var r = Math.max( 3, Math.min( 10 / Math.sqrt(resolution), 10 )) * (props['offline'] > 2 * colors[colors.length-1][0] ? 0.5 : 1.0);
     if (props['uplink']) {
@@ -35,7 +35,7 @@ function style_node(feature,resolution) {
     } else {
         var img = new ol.style.Circle({ radius: r });
     }
-    img.setFill( new ol.style.Fill({ color: icolor( props['offline'], colors ) }) );
+    img.setFill( new ol.style.Fill({ color: "rgba(" + icolor( props['offline'], colors ).join() + ")" }) );
     img.setStroke( new ol.style.Stroke({ color: '#222222' }) );
     return new ol.style.Style({ image: img });
 };

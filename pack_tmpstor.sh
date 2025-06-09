14fce87242c2976bc6f966b99e083e350e2969e7
root <root@kira.local>
2025-06-09 11:05:03 +0200

    added manual selection of month to pack

--- pack_tmpstor.sh
+++ pack_tmpstor.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-LM=`date --date "last month" +%Y-%m`
+if [ "$1" != "" ]; then
+    LM="$1"
+else
+    LM=`date --date "last month" +%Y-%m`
+fi
 cd "`dirname $0`"
 a="tmpstor_${LM}.tbz"
 num=`ls tmpstor/.${LM}-*.zip 2> /dev/null | wc -l`
 if [ $num -gt 0 -a ! -e "$a" ] ; then
     echo $a
     ls -l tmpstor/.${LM}-*.zip
-    tar -cjpf "$a" tmpstor/.${LM}-*.zip && rm tmpstor/.${LM}-*.zip
+    tar -cvjpf "$a" tmpstor/.${LM}-*.zip && rm tmpstor/.${LM}-*.zip
 fi
